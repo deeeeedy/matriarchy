a3c7ad69681206dd7f42b6cd30600ac2f2292604
dar-sn <dasha.sntn@gmail.com>
2023-06-28 20:04:27 +0500

    improve Customer class

--- src/main/java/ButtonEvent.java
+++ src/main/java/ButtonEvent.java
@@ -1,5 +1,3 @@
-package src.main.java;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,5 +1,3 @@
-package src.main.java;
-
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
