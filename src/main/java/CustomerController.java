2d808690cf2ca6b7dd7fd3218c39be4421681f46
dar-sn <dasha.sntn@gmail.com>
2023-06-28 22:07:22 +0500

    improved Customer Controller

--- src/main/java/CustomerController.java
+++ src/main/java/CustomerController.java
@@ -35,8 +35,35 @@ public class CustomerController {
         // Инициализация таблицы адресатов с двумя столбцами.
         firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
         lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
-    }
 
+        // Очистка дополнительной информации об адресате.
+        showPersonDetails(null);
+
+        // Слушаем изменения выбора, и при изменении отображаем
+        // дополнительную информацию об адресате.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+    private void showPersonDetails(Customer person) {
+        if (person != null) {
+            // Заполняем метки информацией из объекта person.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            carNumberLabel.setText(Integer.toString(person.getCarNumber()));
+            carModelLabel.setText(person.getCarModel());
+            phoneNumberLabel.setText(person.getPhoneNumber());
+            clientIdLabel.setText(Integer.toString(person.getClientId()));
+
+        } else {
+            // Если Person = null, то убираем весь текст.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            carNumberLabel.setText("");
+            carModelLabel.setText("");
+            phoneNumberLabel.setText("");
+            clientIdLabel.setText("");
+        }
+    }
     public void setMainApp(MainCustomer mainApp) {
         this.mainApp = mainApp;
 
