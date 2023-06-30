ba4bffece7db52541f81b8a57f3e49851af238af
Mariya <mashulya_pihtovnikova.04@mail.ru>
2023-06-30 18:22:41 +0500

    кнопка

--- Tyre/src/main/java/com/example/tyre/HelloApplication.java
+++ Tyre/src/main/java/com/example/tyre/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Шиномонтажка");
         primaryStage.setScene(new Scene(root, primaryStage.getWidth(), primaryStage.getHeight()));
         primaryStage.show();
     }
