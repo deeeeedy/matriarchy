14fd27256e80298a0dd1508a26039f18110b7717
dar-sn <dasha.sntn@gmail.com>
2023-07-01 11:18:58 +0500

    работают кнопки режим работы и открыть список клиентов

--- src/main/java/com/example/tyre/HelloController.java
+++ src/main/java/com/example/tyre/HelloController.java
@@ -3,16 +3,54 @@ package com.example.tyre;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HelloController {
 
     @FXML
-    private Button button;
+    private Button timeTable;
+    @FXML
+    private Button openCustomerTable;
+
 
     @FXML
     public void initialize(){
-        button.setOnMouseClicked(event -> {
+        timeTable.setOnMouseClicked(event -> {
+            timeTable.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("timetable.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setTitle("Время работы");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        }
+        );
+        openCustomerTable.setOnMouseClicked(event -> {
+            openCustomerTable.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("CustomerTable.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setTitle("Список клиентов");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
     }
 
---
+++ src/main/java/com/example/tyre/TimetableController.java
@@ -0,0 +1,4 @@
+package com.example.tyre;
+
+public class TimetableController {
+}
