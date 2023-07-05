c115d57a48c5b6dc2f43745e368666dbc7aff4b4
dar-sn <dasha.sntn@gmail.com>
2023-07-05 11:19:45 +0500

    добавлена дата

--- src/main/java/com/example/tyre/TimetableController.java
+++ src/main/java/com/example/tyre/TimetableController.java
@@ -1,8 +1,13 @@
 package com.example.tyre;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TimetableController {
     @FXML
@@ -10,8 +15,12 @@ public class TimetableController {
     @FXML
     private AnchorPane backgr;
     @FXML
+    private Label dateLabel;
+    private String date = new SimpleStringProperty(new SimpleDateFormat("dd-MM-yyyy").format(new Date())).getValue();
+    @FXML
     void initialize() {
             backgr.setStyle(HelloController.getStyle());
+            dateLabel.setText(date);
 
         exit.setOnMouseClicked(event -> {
             exit.getScene().getWindow().hide();
